dc50eea550d087622ad44f1251ef8a38f50148cf
Jay Mantuhac <gardelioj.mantuhac@gmail.com>
2022-03-15 23:07:02 -0700

    Added combination of 2019/2020 dataset and 2018 dataset - forms full dataset

--- Dataset Merging.R
+++ Dataset Merging.R
@@ -100,8 +100,65 @@ data_us_2019_sample[, F_CA_OMPHA := NA]
 data_us_2019_sample[, F_CA_GASTRO := NA]
 
 #Combine 2019 and 2020 datasets with imputed columns
-combined_2019_2020_dataset <- rbind(data_us_2019_sample, data_us_2020_sample)
+combined_2019_2020_sample <- rbind(data_us_2019_sample, data_us_2020_sample)
 
-#Note (3/15): Repeat column imputation/combination above for 2018 dataset and 2019/2020 combined dataset
-setdiff(colnames(data_us_2018), colnames(combined_2019_2020_dataset))
-setdiff(colnames(combined_2019_2020_dataset), colnames(data_us_2018))
+#Missing column imputation for 2018 dataset and 2019/2020 combined dataset
+setdiff(colnames(data_us_2018), colnames(combined_2019_2020_sample))
+setdiff(colnames(combined_2019_2020_sample), colnames(data_us_2018))
+
+combined_2019_2020_sample[, FBRACE := NA]
+combined_2019_2020_sample[, PWGT_R := NA]
+combined_2019_2020_sample[, IP_HEPATB := NA]
+combined_2019_2020_sample[, IP_HEPATC := NA]
+combined_2019_2020_sample[, ME_PRES := NA]
+combined_2019_2020_sample[, ME_ROUT := NA]
+combined_2019_2020_sample[, ME_TRIAL := NA]
+combined_2019_2020_sample[, F_MM_ := NA]
+combined_2019_2020_sample[, F_AB_NICU := NA]
+combined_2019_2020_sample[, CA_DOWNS := NA]
+combined_2019_2020_sample[, UBFACIL := NA]
+combined_2019_2020_sample[, URF_DIAB := NA]
+combined_2019_2020_sample[, URF_CHYPER := NA]
+combined_2019_2020_sample[, URF_PHYPER := NA]
+combined_2019_2020_sample[, URF_ECLAM := NA]
+combined_2019_2020_sample[, UME_FORCEP := NA]
+combined_2019_2020_sample[, UME_VAC := NA]
+combined_2019_2020_sample[, UOP_INDUC := NA]
+combined_2019_2020_sample[, ULD_BREECH := NA]
+combined_2019_2020_sample[, UCA_ANEN := NA]
+combined_2019_2020_sample[, UCA_SPINA := NA]
+combined_2019_2020_sample[, UCA_OMPHA := NA]
+combined_2019_2020_sample[, UCA_CLEFTLP := NA]
+combined_2019_2020_sample[, UCA_HERNIA := NA]
+combined_2019_2020_sample[, UCA_DOWNS := NA]
+
+data_us_2018_sample[, MHISPX := NA]
+data_us_2018_sample[, FHISPX := NA]
+data_us_2018_sample[, F_FEDUC := NA]
+data_us_2018_sample[, F_RF_INFT := NA]
+data_us_2018_sample[, OBSTETRIC_PROCEDURES := NA]
+data_us_2018_sample[, CHARACTERISTICS_OF_LABOR_AND_DELIVERY := NA]
+data_us_2018_sample[, MATERNAL_MORBIDITY := NA]
+data_us_2018_sample[, F_MM__PLAC := NA]
+data_us_2018_sample[, ABNORMAL_CONDITIONS_OF_THE_NEWBORN := NA]
+data_us_2018_sample[, F_AB_NIUC := NA]
+data_us_2018_sample[, CA_DOWN := NA]
+data_us_2018_sample[, OCTERR := NA]
+data_us_2018_sample[, OCNTYFIPS := NA]
+data_us_2018_sample[, OCNTYPOP := NA]
+data_us_2018_sample[, MBCNTRY := NA]
+data_us_2018_sample[, MRCNTRY := NA]
+data_us_2018_sample[, MRTERR := NA]
+data_us_2018_sample[, RCNTY := NA]
+data_us_2018_sample[, RCNTY_POP := NA]
+data_us_2018_sample[, RCITY_POP := NA]
+data_us_2018_sample[, RECTYPE := NA]
+data_us_2018_sample[, RISK_FACTORS := NA]
+data_us_2018_sample[, INFECTIONS_PRESENT := NA]
+data_us_2018_sample[, IP_HEPB := NA]
+data_us_2018_sample[, IP_HEPC := NA]
+data_us_2018_sample[, CONGENITAL_ANOMALIES_OF_THE_NEWBORN_ := NA]
+
+#Forms full dataset with all 3 years (2018-2020)
+full_dataset_sample <- rbind(combined_2019_2020_sample, data_us_2018_sample)
+
