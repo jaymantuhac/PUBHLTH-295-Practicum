5c210523ad9d23d3a8e765bfbbd7b38ca4c7dba7
Jay Mantuhac <gardelioj.mantuhac@gmail.com>
2022-03-25 18:29:37 -0700

    Changed head() to sample_n() for more randomized dataset sampling

--- [RUN FIRST] Data Loading.R
+++ [RUN FIRST] Data Loading.R
@@ -7,15 +7,15 @@ libraries("car", "data.table", "dplyr", "formattable", "foreign", "forcats", "fo
 #Load datasets
 data_us_2018 <- data.table::fread('/Volumes/GoogleDrive/.shortcut-targets-by-id/1mMR8JnKiUsIMKqX4IrbhCiI5mv5tvNEu/Jay\'s Practicum /Data/2018 Natality Data/natl2018us.csv')
 data_us_2018 <- data.table(data_us_2018)
-data_us_2018_sample <- data.table(head(data_us_2018, 100))
+data_us_2018_sample <- data.table(sample_n(data_us_2018, 100))
 
 data_us_2019 <- data.table::fread('/Volumes/GoogleDrive/.shortcut-targets-by-id/1mMR8JnKiUsIMKqX4IrbhCiI5mv5tvNEu/Jay\'s Practicum /Data/2019 Natality Data/birth_2019_nber_us.csv')
 data_us_2019 <- data.table(data_us_2019)
-data_us_2019_sample <- data.table(head(data_us_2019, 100))
+data_us_2019_sample <- data.table(sample_n(data_us_2019, 100))
 
 data_us_2020 <- data.table::fread('/Volumes/GoogleDrive/.shortcut-targets-by-id/1mMR8JnKiUsIMKqX4IrbhCiI5mv5tvNEu/Jay\'s Practicum /Data/2020 Natality Data/birth_2020_nber_us.csv')
 data_us_2020 <- data.table(data_us_2020)
-data_us_2020_sample <- data.table(head(data_us_2020, 100))
+data_us_2020_sample <- data.table(sample_n(data_us_2020, 100))
 
 data_us_2018_2020_combined <- data.table::fread('/Volumes/GoogleDrive/.shortcut-targets-by-id/1mMR8JnKiUsIMKqX4IrbhCiI5mv5tvNEu/Jay\'s Practicum /Data/2018_2020_combined_data.csv')
 data_us_2018_2020_combined <- data.table(data_us_2018_2020_combined)
